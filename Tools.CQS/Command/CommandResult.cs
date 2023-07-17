7773a71de4de02a52db480dd3987c39400b241f0
Denys Desmecht <d.desmecht@stag.technofuturtic.education>
2023-07-17 10:18:08 +0200

    Ajoutez des fichiers projet.

---
+++ Tools.CQS/Command/CommandResult.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tools.CQS.Command
+{
+    internal class CommandResult : ICommandResult
+    {
+        public bool IsSuccess { get; init; }
+        public bool IsFailure { get => !IsSuccess; }
+        public string? Message { get; init; } 
+
+        internal CommandResult(bool isSucess, string? message = null)
+        {
+            IsSuccess = isSucess;
+            Message = message;
+        }
+
+
+    }
+}
---
+++ Tools.CQS/Command/ICommandResult.cs
@@ -0,0 +1,12 @@
+﻿namespace Tools.CQS.Command
+{
+    public interface ICommandResult
+    {
+        static ICommandResult Success()
+        {
+            return new CommandResult(true);
+        }
+
+
+    }
+}
